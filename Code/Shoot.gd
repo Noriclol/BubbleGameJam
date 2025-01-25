b419d85c006c859260d819a7b2a25f538a4fe848
Doğaç Mengül <dogacmengul@yahoo.com>
2025-01-25 23:20:41 +0100

    bubble counter added in shoot function

--- Code/Shoot.gd
+++ Code/Shoot.gd
@@ -3,7 +3,9 @@ class_name Shoot
 
 @onready var projectile_scene = preload("res://Scenes/Projectile/projectile.tscn")
 
-func shoot(muzzle : Muzzle) -> void:
-	var projectile = projectile_scene.instantiate()
-	muzzle.add_child(projectile)
-	projectile.transform = muzzle.transform
+func shoot(muzzle : Muzzle):
+    if (Global.player.bubbles >= 1):
+        Global.player.bubbles -= 1
+        var projectile = projectile_scene.instantiate()
+        muzzle.add_child(projectile)
+        projectile.transform = muzzle.transform
