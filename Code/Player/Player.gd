11d6cbe9b1420feeb8017d730d8545a9d1e75e32
Noriclol <maxv.personligt@gmail.com>
2025-01-25 14:21:56 +0100

    add: player stuff

--- Code/Player/Player.gd
+++ Code/Player/Player.gd
@@ -11,8 +11,27 @@ signal jumped_on_trail(trail : Trail)
 func _init() -> void:
 	Global.player = self
 	
+func _ready() -> void:
+	jumped_on_trail.connect(on_jumped_on_trail)
 	
+
 func _physics_process(delta: float) -> void:
-	global_position.x = path_follow.global_position.x
-	global_position.z = path_follow.global_position.z
-	rotation = path_follow.rotation
+	if path_follow:
+		global_position.x = path_follow.global_position.x
+		global_position.z = path_follow.global_position.z
+		rotation = path_follow.rotation
+		
+	
+	if (Global.player.ground_check.is_colliding()):
+		var collider =  Global.player.ground_check.get_collider(1)
+		if collider and collider.is_in_group("ground"):
+			print("collided with collider")
+			var csg_polygon = collider as CSGPolygon3D
+			var path_node = csg_polygon.path_node as Path3D
+			var path_follower = path_node.get_child(0) as PathFollow3D
+			path_follow = path_follower
+			#jumped_on_trail.emit(trail)
+			#pass
+			
+func on_jumped_on_trail(trail:Trail):
+	print("Jumped on trail "+ trail.name)
--- Code/Player/PlayerMovement.gd
+++ Code/Player/PlayerMovement.gd
@@ -3,16 +3,35 @@ class_name PlayerMovement
 
 @export var move_force : float = 10
 @export var jump_force : float = 50
-
-var output_delta : float
-
 var jump_counter : int = 0
+
+var input_movement : int
+
+var on_trail : bool
+
 
 
 func _process(delta):
 	pass
 
 func _physics_process(delta):
+	
+	if on_trail:
+		#follow a pathfollow3d
+		pass
+	else:
+		#use its own rigidbody
+		pass
+	
+	
+func process_input():
+	pass
+	
+	
+	
+	
+	
+	
 	# Shapecast3d ground check
 	if ( Global.player.ground_check.is_colliding()):
 		var collider =  Global.player.ground_check.get_collider(1)
@@ -20,21 +39,21 @@ func _physics_process(delta):
 			print("Grounded")
 			jump_counter = 0
 	
-	# Calculate output Delta
-	if Input.is_action_pressed("Move Left") and Global.player:
-		print("Player Left")
-		output_delta = move_force
-		#Global.player.apply_force(Vector3.FORWARD * move_force)
-	elif Input.is_action_pressed("Move Right") and Global.player:
-		print("Player Right")
-		output_delta = - move_force
-		#Global.player.apply_force(Vector3.BACK * move_force)
-	else: 
-		output_delta = 0
+	## Calculate output Delta
+	#if Input.is_action_pressed("Move Left") and Global.player:
+		#print("Player Left")
+		#output_delta = move_force
+		##Global.player.apply_force(Vector3.FORWARD * move_force)
+	#elif Input.is_action_pressed("Move Right") and Global.player:
+		#print("Player Right")
+		#output_delta = - move_force
+		##Global.player.apply_force(Vector3.BACK * move_force)
+	#else: 
+		#output_delta = 0
 		
 	# Apply Output Delta
-	if output_delta != 0:
-		Global.player.path_follow.progress  += output_delta * delta
+	#if output_delta != 0 and Global.player.path_follow:
+		#Global.player.path_follow.progress  += output_delta * delta
 	
 	if Input.is_action_just_pressed("Jump"):
 		if jump_counter < 1:
