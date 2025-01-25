7780aeb2a5b2d0cf2c3f7bbe81ac431666a81ecb
Doğaç Mengül <dogacmengul@yahoo.com>
2025-01-25 21:00:44 +0100

    shooting projectiles have implemented

--- Code/Projectile.gd
+++ Code/Projectile.gd
@@ -5,7 +5,7 @@ var distance : float = 0
 var max_distance : float = 100
 
 func _physics_process(delta):
-	var movement = global_transform.basis.z * projectile_speed * delta
+	var movement = -global_transform.basis.z * projectile_speed * delta
 	global_transform.origin += movement
 	distance += movement.length()
 
