7ccdf9614b96f211328070fb402fae154a173f4c
Doğaç Mengül <dogacmengul@yahoo.com>
2025-01-25 20:42:54 +0100

    projectile shoot implementation

---
+++ Code/Projectile.gd
@@ -0,0 +1,14 @@
+extends RigidBody3D
+
+@export var projectile_speed : float = 10
+var distance : float = 0
+var max_distance : float = 100
+
+func _physics_process(delta):
+	var movement = global_transform.basis.z * projectile_speed * delta
+	global_transform.origin += movement
+	distance += movement.length()
+
+	if distance > max_distance:
+		queue_free()
+		print("Projectile Destroyed")
--- Code/Shoot.gd
+++ Code/Shoot.gd
@@ -1,18 +1,19 @@
 extends Node
 class_name Shoot
 
-func shoot(muzzle : Muzzle) -> void:
-    var projectile = Projectile.instantiate()
-    add_child(projectile)
-    projectile.transform = muzzle.transform
+@onready var projectile_scene = preload("res://Scenes/Projectile/projectile.tscn")
 
-    var hit = $RayCast.get_collider()
-    if hit:
-        if hit is Health:
-            var health = hit as Health
-            health.apply_damage(10)
-        else:
-            print("Hit something else")
-    else:
-        print("Hit nothing")
-    
+func shoot(muzzle : Muzzle) -> void:
+	var projectile = projectile_scene.instantiate()
+	muzzle.add_child(projectile)
+	projectile.transform = muzzle.transform
+
+	#var hit = $RayCast.get_collider()
+	#if hit:
+	#	if hit is Health:
+	#		var health = hit as Health
+	#		health.apply_damage(10)
+	#	else:
+	#		print("Hit something else")
+	#else:
+	#	print("Hit nothing")
